feba1cdf562f9d414a02137d849d996484eda24a
26433 <2643308978@qq.com>
2023-09-02 11:15:42 +1200

    Version 1.0

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -2,25 +2,14 @@
 const userDao = require("../modules/users-dao.js");
 
 async function addUserToLocals(req, res, next) {
-  //获取客户端cookies
   const user = await userDao.retrieveUserWithAuthToken(req.cookies.authToken);
 
   res.locals.user = user;
 
   next();
 }
-    
-
-
-// function VerifyAuthenticated(req, res, next) {
-//   if (res.locals.user) {
-//     next();
-//   } else {
-//     res.redirect("./login");
-//   }
-// }
+  
 
 module.exports = {
   addUserToLocals
-  // VerifyAuthenticated
 };
